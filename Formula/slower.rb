20f7879db8c78034ea7d87e56c9ce1289e3661f1
Leo Voon <voonlihhaur@gmail.com>
2026-02-06 17:04:50 +0800

    slower 1.0.2

--- Formula/slower.rb
+++ Formula/slower.rb
@@ -1,8 +1,8 @@
 class Slower < Formula
   desc "Periodic walk reminder"
   homepage "https://github.com/leovoon/slower"
-  url "https://github.com/leovoon/slower/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "958b9179a4a35c081634975bfc85e29e753dc644f832696aa8e3e5d59ae3233a"
+  url "https://github.com/leovoon/slower/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "8b515c48f6c044fc33fc4adfa347879c85bbb8e4accede37275f4284747ece6c"
   license "MIT"
 
   depends_on :macos
